9b93e2522cedb8c88455786432a3b82097ef42a0
Salman* at_github <salman1m22@gmail.com>
2021-01-30 02:10:20 +0600

    basic maven example of a java appication

---
+++ src/main/java/com/bk/app/App.java
@@ -0,0 +1,10 @@
+package com.bk.app;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Console Print : Java Maven App Created");
+    }
+}
---
+++ src/test/java/com/bk/app/AppTest.java
@@ -0,0 +1,18 @@
+package com.bk.app;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
